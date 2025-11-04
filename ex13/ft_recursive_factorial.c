42916bb15a37e2543493699d05b28f0cff9b569a
Anselme Bounouar <abounoua@z4r3p6.42lyon.fr>
2025-11-04 15:58:41 +0100

    ft recursive factorial

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,13 +6,13 @@
 /*   By: abounoua <abounoua@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 15:41:40 by abounoua          #+#    #+#             */
-/*   Updated: 2025/11/04 15:54:48 by abounoua         ###   ########lyon.fr   */
+/*   Updated: 2025/11/04 15:58:31 by abounoua         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0)
+	if (nb < 0 || nb > 12)
 		return (0);
 	if (nb == 0)
 		return (1);
