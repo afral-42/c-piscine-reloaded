fd5829b7b66c505f62bb794e20742976e00c9335
Anselme Bounouar <abounoua@z4r3p6.42lyon.fr>
2025-11-04 15:54:29 +0100

    pitie la moulinette arrete de faire de la merde

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: abounoua <abounoua@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 15:27:03 by abounoua          #+#    #+#             */
-/*   Updated: 2025/11/03 16:48:00 by abounoua         ###   ########lyon.fr   */
+/*   Updated: 2025/11/04 15:54:14 by abounoua         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,3 @@ int	ft_iterative_factorial(int nb)
 	}
 	return (result);
 }
-/*
-#include <stdio.h>
-int	main(void)
-{
-	printf("%d\n", ft_iterative_factorial(10));
-	printf("%d\n", ft_iterative_factorial(5));
-	printf("%d\n", ft_iterative_factorial(2));
-	printf("%d\n", ft_iterative_factorial(-10));
-}*/
