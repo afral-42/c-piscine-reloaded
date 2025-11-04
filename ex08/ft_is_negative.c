b0ec1c576a81d7d0a938ed6d1d5ab8821cd4c54f
Anselme Bounouar <abounoua@z4r3p6.42lyon.fr>
2025-11-04 15:02:10 +0100

    correctifs

--- ex08/ft_is_negative.c
+++ ex08/ft_is_negative.c
@@ -6,7 +6,7 @@
 /*   By: abounoua <abounoua@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 14:50:13 by abounoua          #+#    #+#             */
-/*   Updated: 2025/11/04 14:55:25 by abounoua         ###   ########lyon.fr   */
+/*   Updated: 2025/11/04 15:02:00 by abounoua         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@ void	ft_putchar(char c);
 void	ft_is_negative(int n)
 {
 	if (n < 0)
-		ft_putchar('N')
+		ft_putchar('N');
 	else
-		ft_putchar('P')
+		ft_putchar('P');
 }
 /*
 int	main(void)
