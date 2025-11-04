4c173a0fcd6c8143f95bcf3178311418f6e4af72
Anselme Bounouar <abounoua@z4r3p6.42lyon.fr>
2025-11-04 15:22:15 +0100

    Ft point corrige

--- ex23/ft_point.h
+++ ex23/ft_point.h
@@ -6,12 +6,19 @@
 /*   By: abounoua <abounoua@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 09:33:09 by abounoua          #+#    #+#             */
-/*   Updated: 2025/11/04 09:38:59 by abounoua         ###   ########lyon.fr   */
+/*   Updated: 2025/11/04 15:22:03 by abounoua         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
 
 typedef struct s_point
 {
 	int	x;
 	int	y;
 }	t_point;
+
+void set_point(t_point *point);
+
+#endif
